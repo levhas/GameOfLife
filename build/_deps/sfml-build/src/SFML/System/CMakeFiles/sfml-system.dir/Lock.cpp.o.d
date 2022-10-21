032043f3992d37796c47cd725a5ac72cae64b76c
leevi hassila <ljhass@utu.fi>
2022-10-21 23:24:39 +0300

    removed build dir, made build script

--- build/_deps/sfml-build/src/SFML/System/CMakeFiles/sfml-system.dir/Lock.cpp.o.d
+++
@@ -1,8 +0,0 @@
-_deps/sfml-build/src/SFML/System/CMakeFiles/sfml-system.dir/Lock.cpp.o: \
- /home/wadaaaa/Documents/GameOfLife/build/_deps/sfml-src/src/SFML/System/Lock.cpp \
- /usr/include/stdc-predef.h \
- /home/wadaaaa/Documents/GameOfLife/build/_deps/sfml-src/include/SFML/System/Lock.hpp \
- /home/wadaaaa/Documents/GameOfLife/build/_deps/sfml-src/include/SFML/System/Export.hpp \
- /home/wadaaaa/Documents/GameOfLife/build/_deps/sfml-src/include/SFML/Config.hpp \
- /home/wadaaaa/Documents/GameOfLife/build/_deps/sfml-src/include/SFML/System/NonCopyable.hpp \
- /home/wadaaaa/Documents/GameOfLife/build/_deps/sfml-src/include/SFML/System/Mutex.hpp
